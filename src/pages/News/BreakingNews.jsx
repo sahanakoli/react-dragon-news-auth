4c4f1d84bd133a7708f4dc48268f3f2af55bdfaa
Sahana Koli <sahanasultanakoli@gmail.com>
2023-10-05 16:21:29 +0600

    added breaking news slider using react marquee and navbar

--- src/pages/News/BreakingNews.jsx
+++ src/pages/News/BreakingNews.jsx
@@ -1,10 +1,15 @@
-
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
 
 const BreakingNews = () => {
     return (
-        <div className="flex">
+        <div className="flex bg-base-200 mt-6">
            <button className=" btn btn-error">Breaking News</button>
-           
+           <Marquee pauseOnHover={true} speed={150}>
+           <Link className="mr-12" to="/">I can be a React component, multiple React components.....</Link>
+           <Link className="mr-12" to="/">I can be a React component, multiple React components.....</Link>
+           <Link className="mr-12" to="/">I can be a React component, multiple React components.....</Link>
+           </Marquee> 
         </div>
     );
 };
